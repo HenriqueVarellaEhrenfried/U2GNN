74f077706ba0d7eb8ec031b6e9162dd3a50946ca
Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>
2021-03-04 17:26:59 -0300

    Add file to run on cluster
    Signed-off-by: Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>

---
+++ U2GNN_pytorch/Run_U2GNN_Cluster.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -t 7-00:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH -o U2GNN_SPACY_Ohsumed.log
+#SBATCH --job-name=hve_U2GNN
+#SBATCH -p 7d
+#SBATCH -n 1
+#SBATCH -N 1
+
+source $HOME/.virtualenvs/U2GNN/bin/activate
+
+python -V
+
+pwd
+
+srun time python script_experiments.py
